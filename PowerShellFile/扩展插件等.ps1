f3db55ac06e8bd1860e239ffa7d14f687dd32e6a
hailun.zhang <hailun.zhang@toshiba-tih.com>
2019-05-02 17:29:34 +0800

    add file

--- PowerShellFile/扩展插件等.ps1
+++ PowerShellFile/扩展插件等.ps1
@@ -36,3 +36,5 @@ Get-Process |Select-Object -First 1 -Last 10
 <#
 Get- Content .\computers. txt | Get- Service 当 运行 Get- Content 命令 时， 它 会 将 文本 文件 中的 计算机 名称 放入 管道 中。 之后 PowerShell 再 决定 如何 将 该数 据传 递给 Get- Service 命令。 但 PowerShell 一次 只能 使用 单个 参数 接收 传入 数据。 也就是说， PowerShell 必须 决定 由 Get- Service 的 哪个 参数 接收 Get- Content 的 输出 结果。 这个 决定 的 过程 就 称为 管道 参数 绑 定（ Pipeline parameter binding）， 这也 是 本章 主要 讲解 的 内容。 PowerShell 使用 两种 方法 将 Get- Content 的 输出 结果 传入 给 Get- Service 的 某个 参数。 该 Shell 尝试 使用 的 第一 种 方法 称为 ByValue； 如果 这种 方法 行不通， 它将 会 尝试 ByPropertyName。
 #>
+
+
